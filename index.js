b75629426744463d411aaa050c8f0cae8403486b
unknown <22220252@KTNG.AD>
2023-06-29 09:17:29 +0900

    조건추가

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import words from "./words.js";
-let MAX_LENGTH = 6;
+let SAMPLE_WORD_NUM = 20; // 샘플단어 갯수
+let NICK_LENGTH = 6; // 닉네임 길이
+let GEN_TYPE = "1"; // 생성방식
 
 function getRandomInt(min, max) {
   min = Math.ceil(min);
@@ -7,19 +9,31 @@ function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min)) + min; //최댓값은 제외, 최솟값은 포함
 }
 
-function generateRandomNickname(maxLenth) {
+function generateRandomNickname(nickLenth, genType) {
+  //console.log("param:nickLenth,genType:", nickLenth, genType);
   const result = document.querySelector("#result");
   let wordStr = "";
   let letterStr = "";
 
-  for (let i = 0; i < 10; i++) {
+  for (let i = 0; i < SAMPLE_WORD_NUM; i++) {
     let randomWordIdx = getRandomInt(0, words.length);
     wordStr = wordStr + words[randomWordIdx].replace(/[0-9]/g, ""); // 숫자제거
   }
 
-  for (let i = 0; i < maxLenth; i++) {
-    let randomLetter = wordStr.charAt(getRandomInt(0, wordStr.length));
-    letterStr = letterStr + randomLetter;
+  switch (genType) {
+    case "1": // 완전랜덤
+      for (let i = 0; i < nickLenth; i++) {
+        let randomLetter = wordStr.charAt(getRandomInt(0, wordStr.length));
+        letterStr = letterStr + randomLetter;
+      }
+      break;
+    case "2": // 부분단어
+      let indexStart = getRandomInt(0, NICK_LENGTH);
+      let indexEnd = indexStart + Number(nickLenth);
+      letterStr = wordStr.substring(indexStart, indexEnd);
+      break;
+    default:
+    //
   }
 
   result.innerHTML = letterStr;
@@ -27,15 +41,21 @@ function generateRandomNickname(maxLenth) {
 
 function init() {
   const numberSelect = document.querySelector("#number-select");
+  const gentypeSelect = document.querySelector("#gentype-select");
   const btnChoice = document.querySelector("#btn-choice");
 
   numberSelect.addEventListener("change", function () {
     var option = numberSelect.options[numberSelect.selectedIndex];
-    MAX_LENGTH = option.value;
+    NICK_LENGTH = option.value;
+  });
+
+  gentypeSelect.addEventListener("change", function () {
+    var option = gentypeSelect.options[gentypeSelect.selectedIndex];
+    GEN_TYPE = option.value;
   });
 
   btnChoice.addEventListener("click", function () {
-    generateRandomNickname(MAX_LENGTH);
+    generateRandomNickname(NICK_LENGTH, GEN_TYPE);
   });
 }
 
